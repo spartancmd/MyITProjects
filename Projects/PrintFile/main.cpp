d3cbea83717fa2b9d55ff06aafaf7aad7eeb2c75
Deniel <kankan8@bk.ru>
2024-10-17 20:58:53 +0200

    Written the function print_file_from_the_end()
    The function print_file_from_the_end() has now been written and tested.
    The problem to be fixed is that the programm seems not to run if I use
    methods such as .tellg() and .seekg() (both in the function get_cnt_of_lines_in_file()).
    So this function is commented at the moment.
    
    Also, a function that checks if the arguments are written correctly has
    to be written (function check_cla).

--- Projects/PrintFile/main.cpp
+++ Projects/PrintFile/main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cstring>
-#include <algorithm>
+#include <vector>
 
 // -l, --lines=n вывести только n (только положительное) первых(последних) строк файл (необязательный аргумент, по-умолчанию выводим все)
 // -t, --tail вывод n последний строк файла (необязательный аргумент)
@@ -36,17 +35,45 @@ Parameters process_cl_args(unsigned argc, std::string* args)   // returns a stru
     return params;
 }
 
+// unsigned long int get_cnt_lines_in_file(std::ifstream& file)
+// {
+//     if(!file.is_open()) // If the file is not opened
+//         return 0;       // there are no lines
+
+//     unsigned lines = 1;
+//     char ch = 0;
+//     std::streampos start_pos_of_file = file.tellg();
+
+//     while(file.get(ch))
+//     {
+//         if (ch == '\n')
+//             lines++;
+//     }
+    
+//     file.clear();
+//     file.seekg(start_pos_of_file);
+
+//     return lines;
+// }
+
 void print_file_from_end(Parameters& params, std::ifstream& file)
 {
+    unsigned long lines_in_file = 4 /*get_cnt_lines_in_file(file)*/;
+    std::string read_line{};
 
+
+    for(auto line_cnt = 1; (line_cnt <= (lines_in_file - params.lines)) && getline(file, read_line); line_cnt++); // skip the first unneeded lines (= "lines in the whole file" - "the amount of lines from bottom of the file")
+                                                                                                                            // or until we reach the end of the file
+    while(getline(file, read_line))     // Printing the last lines in the console
+        std::cout << read_line << params.delimiter;
 }
 
 void print_file(Parameters& params, std::ifstream& file)
 {
-    std::string line{}; // line where the interpreted line from the file gets saved
+    std::string read_line{}; // line where the interpreted line from the file gets saved
 
-    for(auto line_cnt = 1; line_cnt <= params.lines && getline(file, line); line_cnt++)
-        std::cout << line << params.delimiter;
+    for(auto line_cnt = 1; line_cnt <= params.lines && getline(file, read_line); line_cnt++)
+        std::cout << read_line << params.delimiter;
 
 }
 
@@ -54,16 +81,15 @@ int process_params(Parameters& params, std::string path)
 {
     if(params.tail) 
     {
-        std::ifstream file{path, std::ios::ate};
+        std::ifstream file{path};
 
         if(!file.is_open()) // if returns false print error and leave
         {
             std::cerr << "An error occured while opening the file!" << std::endl;
             return 1;   
         }
-
         print_file_from_end(params, file);
-
+        
         file.close();
     }
     else 
@@ -94,12 +120,27 @@ std::string* parse_cla(int argc, char* argv[])
     return args;
 }
 
+int check_cla(std::string* args)
+{
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
     std::string* args = parse_cla(argc, argv); // should return arguments in string format // cla - command line arguments 
     std::string path = args[argc - 1];
+    Parameters params{};
 
-    Parameters params = process_cl_args(argc - 1, args); // args.length() - 1 because we don't need the file path that is at the end
+    if (check_cla(args)) // returns 0 if everything is fine and 1 if not
+    {
+        std::cerr << "Error: Launched the programm with wrong arguments." << std::endl;
+        std::cout << "type: printFile.exe [options] <path of the text file>" << std::endl;
+        std::cout << "options:\n -l [lines] / --lines=[lines]\t sets the number of lines\n-d [symbol] / --delimiter=[symbol]\t sets the delimeter\n-t / --tail\t print file from the bottom" << std::endl;
+
+        return 1;
+    }
+    
+    params = process_cl_args(argc - 1, args); // args.length() - 1 because we don't need the file path that is at the end
 
     process_params(params, path);
 
