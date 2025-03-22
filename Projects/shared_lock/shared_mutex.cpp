555aee98bf7475f1a2bb4dbc0e162612b9305941
Deniel <strummerdeniel@gmail.com>
2025-03-22 20:15:08 +0100

    Change of data types and bit shifting rework

--- Projects/shared_lock/shared_mutex.cpp
+++ Projects/shared_lock/shared_mutex.cpp
@@ -1,9 +1,9 @@
 #include "shared_mutex.hpp"
 
-inline long Shared_mutex::get_readers() {
+inline uint64_t Shared_mutex::get_readers() {
     // if left bit is 1
-    if (status >= (1 << 64)) 
-        return status - (1 << 64);
+    if (status >= (1ull << 64)) 
+        return status - (1ull << 64);
     return status;
 }
 
@@ -15,8 +15,8 @@ Shared_mutex::~Shared_mutex()
 
 void Shared_mutex::lock() {
     // wait untill there're no readers and writer (with CAS)
-    long target = 0;
-    long desired = 1 << 64;
+    uint64_t target = 0;
+    uint64_t desired = 1ull << 64;
 
     do {
         target = 0;
@@ -25,14 +25,14 @@ void Shared_mutex::lock() {
 
 void Shared_mutex::unlock() {
     // if there's a writer
-    if (status >= (1 << 64)){
-        status -= 1 << 64;
+    if (status >= (1ull << 64)){
+        status -= 1ull << 64;
     }
 }
 
 void Shared_mutex::lock_shared() {
     // wait until there's no writer
-    long target = 0;
+    uint64_t target = 0;
 
     do {
         target = get_readers();
--- Projects/shared_lock/shared_mutex.hpp
+++ Projects/shared_lock/shared_mutex.hpp
@@ -4,10 +4,9 @@
 
 class Shared_mutex {
 private:
-    std::atomic<long> status = 0; // 64 bits. Left bit means if there is a writer. Other right bits the count of readers
+    std::atomic<uint64_t> status = 0; // 64 bits. Left bit means if there is a writer. Other right bits the count of readers
 
-    long get_readers();
-    long isWriter();
+    uint64_t get_readers();
 
 public:
     Shared_mutex();
